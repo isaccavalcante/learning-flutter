61de8c75646d37ffa800e4622ff816ba26e6ecfc
Isac Cavalcante <isaccavalcante@alu.ufc.br>
2019-02-15 23:22:08 -0300

    Add tab bar navigation project

---
+++ projects/tab_navigation/lib/ThirdPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Third extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new Container(
+      child: new Center(
+        child: new Icon(Icons.alternate_email, color: Colors.blueAccent, size: 150.0,)
+      )
+    );
+  }
+}
